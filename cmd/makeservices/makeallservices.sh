c5b6e3a14d061e3f272222df50e2b521188224a6
Çağlar Onur <cglr@google.com>
2020-06-28 19:38:42 -0700

    Add a comment to make it clear that these files are autogenerated.

--- cmd/makeservices/makeallservices.sh
+++ cmd/makeservices/makeallservices.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 declare -a arr=("VirtualLineIn" "GroupRenderingControl" "Queue" "AVTransport" "ConnectionManager" "RenderingControl")
 
 for i in "${arr[@]}"
 do
-    rm -rf ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    mkdir -p ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    go run makeservice.go ${i} /MediaRenderer/${i}/Control /MediaRenderer/${i}/Event ${GOPATH}/src/github.com/szatmary/sonos/cmd/makeservices/xml/${i}1.xml >${i}.go
+    mkdir -p ${DIR}/../../${i}/
+    rm -f ${DIR}/../../${i}/*.go
+    go run ${DIR}/makeservice.go ${i} /MediaRenderer/${i}/Control /MediaRenderer/${i}/Event ${DIR}/xml/${i}1.xml >${i}.go
     go fmt ${i}.go
-    mv ${i}.go ${GOPATH}/src/github.com/szatmary/sonos/${i}/
+    mv ${i}.go ${DIR}/../../${i}/
 done
 
 declare -a arr=("ContentDirectory" "ConnectionManager")
 
 for i in "${arr[@]}"
 do
-    rm -rf ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    mkdir -p ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    go run makeservice.go ${i} /MediaServer/${i}/Control /MediaServer/${i}/Event ${GOPATH}/src/github.com/szatmary/sonos/cmd/makeservices/xml/${i}1.xml >${i}.go
+    mkdir -p ${DIR}/../../${i}/
+    rm -f ${DIR}/../../${i}/*.go
+    go run ${DIR}/makeservice.go ${i} /MediaServer/${i}/Control /MediaServer/${i}/Event ${DIR}/xml/${i}1.xml >${i}.go
     go fmt ${i}.go
-    mv ${i}.go ${GOPATH}/src/github.com/szatmary/sonos/${i}/
+    mv ${i}.go ${DIR}/../../${i}/
 done
 
 declare -a arr=("AlarmClock" "MusicServices" "DeviceProperties" "SystemProperties" "ZoneGroupTopology" "GroupManagement" "QPlay")
 
 for i in "${arr[@]}"
 do
-    rm -rf ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    mkdir -p ${GOPATH}/src/github.com/szatmary/sonos/${i}/
-    go run makeservice.go ${i} /${i}/Control /${i}/Event ${GOPATH}/src/github.com/szatmary/sonos/cmd/makeservices/xml/${i}1.xml >${i}.go
+    mkdir -p ${DIR}/../../${i}/
+    rm -f ${DIR}/../../${i}/*.go
+    go run ${DIR}/makeservice.go ${i} /${i}/Control /${i}/Event ${DIR}/xml/${i}1.xml >${i}.go
     go fmt ${i}.go
-    mv ${i}.go ${GOPATH}/src/github.com/szatmary/sonos/${i}/
+    mv ${i}.go ${DIR}/../../${i}/
 done
